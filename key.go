17a2f32b412086d6967414c455c018173ba5ea9d
jason <jason@zgwit.com>
2024-06-02 12:13:13 +0800

    添加 load store

--- key.go
+++ key.go
@@ -3,6 +3,8 @@ package license
 import (
 	"crypto/ed25519"
 	"encoding/base64"
+	"encoding/json"
+	"os"
 )
 
 const DefaultPublicKey = "J7trVLsKRE2r+jRvCAHxycPjQbdwIm52+YU0jJo1KJM="
@@ -10,6 +12,22 @@ const DefaultPublicKey = "J7trVLsKRE2r+jRvCAHxycPjQbdwIm52+YU0jJo1KJM="
 type Pair struct {
 	PublicKey  string `json:"public_key"`
 	PrivateKey string `json:"private_key"`
+}
+
+func (p *Pair) Store(name string) error {
+	buf, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(name, buf, os.ModePerm)
+}
+
+func (p *Pair) Load(name string) error {
+	buf, err := os.ReadFile(name)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buf, p)
 }
 
 func Generate() (*Pair, error) {
